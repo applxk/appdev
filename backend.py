811ca53f1d9cc9dfe69d0c33a59c4e906a9b369d
applx <154079719+applxk@users.noreply.github.com>
2024-02-01 22:29:42 +0800

    Update backend.py

--- backend.py
+++ backend.py
@@ -8,9 +8,9 @@ def retrieve_chat_messages():
     with shelve.open('chat_messages_db') as db:
         return db.get('messages', [])
 
-def store_amount_spent(amount):
+def store_amount_spent(amounts):
     with shelve.open('amount_db') as db:
-        db['amount'] = amount
+        db['amount'] = amounts
 
 def retrieve_amount_spent():
     with shelve.open('amount_db') as db:
@@ -23,3 +23,13 @@ def store_promo_codes(promo_codes):
 def retrieve_promo_codes():
     with shelve.open('promo_codes_db') as db:
         return db.get('promo_codes', [])
+
+def store_subscribed_email(email):
+    subscribed_emails = retrieve_subscribed_emails()
+    subscribed_emails.append(email)
+    with shelve.open('subscribed_emails_db') as db:
+        db['subscribed_emails'] = subscribed_emails
+
+def retrieve_subscribed_emails():
+    with shelve.open('subscribed_emails_db') as db:
+        return db.get('subscribed_emails', [])
